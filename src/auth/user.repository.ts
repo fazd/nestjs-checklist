89aae192ec33681e0c9e1315f86c6f79f664497d
fabio <fabioz@uninorte.edu.co>
2021-02-12 22:56:34 -0500

    Add hash and salt

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,16 +1,36 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
-
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
 
-    const user = new User();
-    user.username = username;
-    user.password = password;
+    const salt = await bcrypt.genSalt();
 
-    await user.save();
+    const user = new User();
+    const hashPassword = await this.hashPassword(password, salt);
+    user.username = username;
+    user.password = hashPassword;
+    user.salt = salt;
+    try {
+      await user.save();
+    } catch (error) {
+      if (error.code === '23505') {
+        // Duplicate username
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return bcrypt.hash(password, salt);
   }
 }
